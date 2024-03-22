e17ed26915c69b61909934a1c54d2c5ee3b781ef
Kane <szkane@gmail.com>
2024-03-22 18:09:45 +0800

    fix2

--- ImproveWritting.js
+++ ImproveWritting.js
@@ -9,7 +9,7 @@
 //   description: 'Obtain API key from Google Cloud Console'
 // }, {
 //   identifier: prompt, label: 'Improve Writing Prompt', type: string,
-//   defaultValue: "I will give you text content, you will rewrite it and output a better version of my text. Keep the meaning the same. Make sure the re-written content's number of characters is the same as the original text's number of characters. Do not alter the original structure and formatting outlined in any way. Only give me the output and nothing else. Now, using the concepts above, re-write the following text. Respond in the same language variety or dialect of the following text:{input}",
+//   defaultValue: "You will rewrite the text and output a better version of my text. Keep the meaning the same. Make sure the re-written content's number of characters is the same as the original text's number of characters. Do not alter the original structure and formatting outlined in any way.  Now, using the concepts above, re-write the following text. Respond in the same language variety or dialect of the following text:{input}",
 //   description: 'Enter the prompt template using {input} as a placeholder for the text'
 // }]
 
